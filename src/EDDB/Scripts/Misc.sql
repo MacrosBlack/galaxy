4192f10a8e46f0e8af36b93e5d33eb07b0d2d2f0
MacrosBlack <Gert@GERTPC>
2019-01-26 20:50:18 +0100

    Misc

---
+++ src/EDDB/Scripts/Misc.sql
@@ -0,0 +1,9 @@
+﻿select count(*) from tblEDSystemsWithCoordinates
+
+SELECT TOP 10 [Date], DATENAME(dw, [Date]), COUNT(Date) FROM tblEDSystemsWithCoordinates GROUP BY [Date] ORDER BY COUNT([Date]) DESC
+
+select top 1 max([Date]) from tblEDSystemsWithCoordinates
+
+-- truncate table [dbo].[tblEDSystemsWithCoordinates]
+
+-- truncate table [dbo].[tblSystemsProgress]
